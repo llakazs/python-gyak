88327d035c463fa30e84c465cb34668ea0ce2c2f
llakazs <hunyo@holtpont.eu>
2022-05-07 23:46:43 +0200

    v1

---
+++ function.py
@@ -0,0 +1,42 @@
+from list import *
+
+# 1. készíts egy függvényt, ami egy lista paramétert vesz át, és a lista elemeit kiiratja csupa nagybetűvel (egy sorba)
+# hívd meg a függvényt a zöldségeket tartalmazó listával
+
+
+# 2. készíts egy függvényt, ami egy lista paramétert vesz át, és a benne lévő elemeket lecseréli azok nagybetűs változatára
+# hívd meg a függvényt a gyümölcsöket tartalmazó listával, és írasd ki a 4. elemet
+
+
+# 3. készíts egy függvényt, ami egy lista paramétert vesz át, és a benne lévő elemek kisbetűs változatát adja vissza új listában
+# hívd meg a függvényt a gyümölcsöket tartalmazó listával, és írasd ki a 3. elemet
+
+
+# 3. készíts egy függvényt, ami két lista paramétert vesz át, és mindkettőből kiírja az 5-tel osztható indexű elemeket
+# hívd meg a függvényt a gyümölcs és a zöldség listával
+
+
+# 4. készíts egy függvényt, ami két lista paramétert vesz át, mindkettőből kiveszi az 5-tel osztható indexű elemeket,
+# de csak akkor, ha az utolsó karakterük nagy betű, visszaadja az eredményt új listában
+# hívd meg a függvényt a zöldség és a gyümölcs listán, majd irasd ki az eredmény lista 2-4. elemét
+
+
+# 5. készíts egy függvényt, ami egy lista és egy boolean paramétert vár
+# ha a boolean true, rendezd a listát növekvő rend szerint, ha a boolean false, rendezd a listát fordított rendbe
+# hívd meg a függvényt a gyümölcs listán úgy, hogy fordított sorrendbe rakja azt
+
+
+# 6. az elkészített függvényeid megfelelő sorrendben való hívásával írasd ki a gyümölcs lista elemeit
+# csupa nagy betűvel, növekvő sorrendbe rendezve
+
+
+# 7. készíts egy függvényt, ami egy lista és egy string paramétert vár, visszatérési értéke pedig egy új lista legyen,
+# amibe azokat az elemeket válogasd bele, melyek tartalmazzák a megadott string-et 
+
+
+# 8. készíts egy függvényt, ami egy lista paramétert vár, és visszaadja ennek a listának a duplikátum mentes változatát,
+# azaz egy érték csak egyszer szerepelhet benne
+
+
+# 9. az elkészített függvényeid megfelelő sorrendben való hívásával írasd ki a zöldség listából
+# azokat, amelyek tartalmaznak "o" betűt, az eredmény legyen csupa kisbetűvel írva és fordított sorrendbe rendezve
---
+++ list.py
@@ -0,0 +1,31 @@
+fruits = ["alma", "körte", "Barack", "Mandarin", "szilva", "cseresznye", "málna"]
+vegetables = ["paradicsom", "fokhagyma", "Krumpli", "Uborka", "sütőtök", "paradicsom", "Vöröshagyma", "Uborka", "paradicsom", "retek", "paprika", "paradicsom", "lilajhagyma"]
+
+# 1. írasd ki minden második gyümölcsöt
+
+
+# 2. írasd ki az áfonya után felsorolt gyümölcsöket
+
+
+# 3. rendezd a gyümölcsöket tartalmazó listát, írasd ki a 3. és 4. elemet a listából
+
+
+# 4. írd ki, hogy melyik zöldség hányszor szerepel a listában
+
+
+# 5. írasd ki minden harmadik zöldséget
+
+
+# 6. írass ki minden zöldséget csak egyszer
+
+
+# 7. írasd ki a zöldségek első betűjét
+
+
+# 8. rendezd a zöldségek listáját fordított sorrendbe és írasd ki az első 5 elemet
+
+
+# 9. írasd ki az 5. elemet a zöldség listából csupa nagybetűvel
+
+
+# 10. írasd ki, hogy hányszor szerepel a paradicsom a zöldség listában
